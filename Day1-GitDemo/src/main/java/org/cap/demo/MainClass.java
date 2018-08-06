aa4f03100471be8346070fc4378306e0dbfab8e9
sinmural <sinmural@DIN73003036.corp.capgemini.com>
2018-08-06 10:52:54 +0530

    My Second Commit

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -5,10 +5,14 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		greet();
+
 	}
 	
 	public static void greet() {
 		System.out.println("Hello");
+		for(int i = 0; i< 10; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
