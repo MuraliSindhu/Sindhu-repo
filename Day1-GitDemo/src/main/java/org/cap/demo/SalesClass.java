62da00b98181b4f586a706c77771570178f25409
sinmural <sinmural@DIN73003036.corp.capgemini.com>
2018-08-06 12:11:41 +0530

    Sales Class addes

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class SalesClass {
+	
+	int[] arr = {1, 2, 3, 4};
+	
+	public void show() {
+		for(int i = 0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
