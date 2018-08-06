42c00fa1b66dee784211801bfb7e4ab20b8559be
smadhira <smadhira@DIN73002982.corp.capgemini.com>
2018-08-06 14:29:03 +0530

    Commit by deepthi

--- Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -5,9 +5,13 @@ public class SalesClass {
 	int[] arr = {1, 2, 3, 4};
 	
 	public void show() {
+		int sum = 0;
 		for(int i = 0; i<arr.length; i++) {
 			System.out.println(arr[i]);
+			sum += arr[i];
+			
 		}
+		System.out.println(sum);
 	}
 
 }
